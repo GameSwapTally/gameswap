045fb9ce0f9291312fc220cf821c79bc03d6b0de
garrettschmitt <garr.schm@gmail.com>
2016-11-22 12:15:36 -0500

    corrected redirection header if somewone tries to access user page and is not logged in

--- user.php
+++ user.php
@@ -11,7 +11,7 @@ $lastsession = "";
 if(isset($_GET["u"])){
 	$u = preg_replace('#[^a-z0-9]#i', '', $_GET['u']);
 } else {
-    header("location: http://www.yoursite.com");
+    header("location: index.html");
     exit();	
 }
 // Select the member from the users table
